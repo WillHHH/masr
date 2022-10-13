8d80cfe8d1696ab78f0be7cf20b5bd1adf44ea1a
WillHHH <willsihangw@gmail.com>
2022-10-13 17:14:34 -0400

    feat: update map info

--- src/components/sections/MapInfo/MapInfo.tsx
+++ src/components/sections/MapInfo/MapInfo.tsx
@@ -3,7 +3,6 @@ import cx from "classnames";
 import styles from "./MapInfo.module.scss";
 import Reveal from "components/Layout/Reveal";
 import Container from "components/Layout/Container";
-import moment from "moment";
 
 type ContactUsProps = {
   className?: string;
@@ -13,28 +12,12 @@ type ContactUsProps = {
 const MapInfo: FC<ContactUsProps> = ({ className }) => {
   const hours = [
     {
-      day: "Mon",
-      hour: "11am - 5:30pm",
-    },
-    {
-      day: "Tue",
-      hour: "11am - 5:30pm",
-    },
-    {
-      day: "Wed",
-      hour: "11am - 5:30pm",
-    },
-    {
-      day: "Thu",
-      hour: "11am - 5:30pm",
-    },
-    {
-      day: "Fri",
-      hour: "11am - 5:30pm",
+      day: "Mon-Fri",
+      hour: "11am-5:30pm",
     },
     {
       day: "Sat",
-      hour: "12pm - 5pm",
+      hour: "12pm-5pm",
     },
     {
       day: "Sun",
@@ -60,19 +43,18 @@ const MapInfo: FC<ContactUsProps> = ({ className }) => {
       <Container className={cx(styles.base, className)}>
         <Reveal className={styles.info}>
           <h3>GET IN TOUCH</h3>
-          <p>Contact us at</p>
-          <h2>347-265-9189</h2>
+          <h2>
+            <a href="tel:6315909196">631-590-9196</a>
+          </h2>
           <div className={styles.hours}>
             {hours.map((v, i) => (
-              <div
-                key={i}
-                className={moment().format("ddd") === v.day && styles.bold}
-              >
+              <div key={i}>
                 <span>{v.day}</span>
                 <span>{v.hour}</span>
               </div>
             ))}
           </div>
+          <img src="/images/wechat.jpeg" alt="QRcode" />
         </Reveal>
       </Container>
     </div>
